f16880b2130f507b6bfefbfed2656187a33915a4
kuanl salokhe <salokhekunal770@gmail.com>
2025-11-19 21:12:32 +0530

    First Push

---
+++ Hello.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main()  {
+   
+    int number = 25;
+    char star = '*';
+    int age = 18;
+    float pi = 3.14;
+
+    int a =30;
+    int A = 40;
+    
+    return 0;
+}
